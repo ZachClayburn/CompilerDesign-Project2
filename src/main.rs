7caf330bf9712241e91cf0043cbef32520b8b516
Zach Clayburn <zachclayburn@gmail.com>
2021-10-26 10:17:16 -0600

    Some dummy code to remove compiler warnings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod scanner;
 mod parser;
 
+use scanner::Scanner;
+use parser::parse;
+
 fn main() {
     println!("Hello, world!");
+    let scan = Scanner::from_text("1 + 1");
+    parse(scan).unwrap();
 }
