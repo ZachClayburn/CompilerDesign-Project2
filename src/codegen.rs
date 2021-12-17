44f759826675c4298ef01cf0e1c7e7b471572691
Zach Clayburn <zachclayburn@gmail.com>
2021-12-16 21:24:32 -0700

    Added constant nums to the compilation

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,8 +1,9 @@
 mod code_file;
 mod symbol_table;
 
-use crate::parser::ast::{PrintExpr, Statement};
+use crate::parser::ast::{Expression, PrintExpr, Statement, TypedVar};
 use code_file::CodeFile;
+use log::info;
 use symbol_table::SymbolTable;
 
 #[derive(Debug, PartialEq, Clone)]
@@ -19,15 +20,22 @@ pub fn generate_assembly(statements: Vec<Statement>) -> Result<String> {
     for statement in statements {
         match statement {
             Statement::Declaration {
-                name_and_type,
-                expression,
-            } => todo!(),
+                name_and_type: TypedVar::Num(name),
+                expression: Expression::NumberLiteral(value),
+            } => {
+                let label = table.new_const_num(name)?;
+                code.rodata.push(format!("{} dd {}", label, value));
+            }
+            Statement::Declaration {
+                name_and_type: _,
+                expression: _,
+            } => info!("Unsupported declaration"),
             Statement::ProcedureDeclaration {
-                name_and_type,
-                params,
-                statements,
-            } => todo!(),
-            Statement::ReturnStatement(_) => todo!(),
+                name_and_type: _,
+                params: _,
+                statements: _,
+            } => info!("unsupported procedure declaration"),
+            Statement::ReturnStatement(_) => info!("Unsupported return statement"),
             Statement::PrintStatement(PrintExpr::String(to_print)) => {
                 let label = table.get_string_label();
                 code.rodata
@@ -39,8 +47,8 @@ pub fn generate_assembly(statements: Vec<Statement>) -> Result<String> {
                     "call printf".to_owned(),
                 ])
             }
-            Statement::PrintStatement(_) => todo!(),
-            Statement::ReadStatement(_) => todo!(),
+            Statement::PrintStatement(_) => info!("Unsupported print statement"),
+            Statement::ReadStatement(_) => info!("Unsupported read statement"),
         }
     }
 
@@ -160,6 +168,48 @@ mod test {
                             mov     edi, stringPrinter
                             mov     eax, 0
                             call    printf
+                            mov     eax, 0
+                            pop     rbp
+                            ret
+            "#});
+        assert_eq!(assembly, expected);
+    }
+
+    #[test]
+    fn programs_can_have_constant_integers() {
+        let statements = vec![
+            Statement::Declaration {
+                name_and_type: TypedVar::Num("a".to_owned()),
+                expression: Expression::NumberLiteral(42),
+            },
+            Statement::Declaration {
+                name_and_type: TypedVar::Num("b".to_owned()),
+                expression: Expression::NumberLiteral(3),
+            },
+        ];
+        let assembly = generate_assembly(statements)
+            .unwrap()
+            .lines()
+            .map(|x| x.to_owned())
+            .collect::<Vec<_>>();
+        let expected = flatten_lines(indoc! {r#"
+            global main
+            extern printf
+            extern scanf
+                            section .rodata
+            numPrinter      db "%d",0x0a,0
+            ishPrinter      db "%f",0x0a,0
+            stringPrinter   db "%s",0x0a,0
+            numReader       db "%d",0
+            ishReader       db "%f",0
+            _num_const_0_a  dd 42
+            _num_const_1_b  dd 3
+                            section .data
+                            section .bss
+                            section .text
+            main:
+                            push    rbp
+                            mov     rbp, rsp
                             mov     eax, 0
                             pop     rbp
                             ret
--- src/codegen/symbol_table.rs
+++ src/codegen/symbol_table.rs
@@ -1,15 +1,27 @@
+use super::{CodeGenError, Result};
+
 pub(super) struct SymbolTable {
     string_count: isize,
+    const_num_count: isize,
 }
 
 impl SymbolTable {
     pub fn new() -> Self {
-        Self { string_count: 0 }
+        Self {
+            string_count: 0,
+            const_num_count: 0,
+        }
     }
 
     pub fn get_string_label(&mut self) -> String {
         let label = format!("_str_{}", self.string_count);
         self.string_count += 1;
         label
+    }
+
+    pub fn new_const_num(&mut self, name: String) -> Result<String> {
+        let label = format!("_num_const_{}_{}", self.const_num_count, name);
+        self.const_num_count += 1;
+        Ok(label)
     }
 }
